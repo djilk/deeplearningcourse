90c1db9f176fa8266f467e79dbd568984d90212e
Dave Jilk <dave@jilk.com>
2016-11-29 12:50:01 -0700

    Copied softmax code from exercise

--- src/deeplearning.py
+++ src/deeplearning.py
@@ -1,6 +1,20 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+"""Softmax."""
 
-if __name__ == "__main__":
-    print "Hello World"
+scores = [3.0, 1.0, 0.2]
+
+import numpy as np
+
+def softmax(x):
+    """Compute softmax values for x."""
+    pass # TODO: Computate and return softmax(x)
+    
+# Plot softmax curves
+import matplotlib.pyplot as plt
+x = np.arange(-2.0, 6.0, 0.1)
+scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+plt.plot(x, softmax(scores).T, linewidth=2)
+plot.show()
