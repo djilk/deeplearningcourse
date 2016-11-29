2fb88005c5ba1e92e94efd1c3c50124c2cc3ed6f
Dave Jilk <dave@jilk.com>
2016-11-29 13:16:47 -0700

    Copied softmax code from exercise

--- src/deeplearning.py
+++ src/deeplearning.py
@@ -9,12 +9,15 @@ import numpy as np
 
 def softmax(x):
     """Compute softmax values for x."""
-    pass # TODO: Computate and return softmax(x)
-    
-# Plot softmax curves
-import matplotlib.pyplot as plt
-x = np.arange(-2.0, 6.0, 0.1)
-scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+    pass # TODO: Compute and return softmax(x)
 
-plt.plot(x, softmax(scores).T, linewidth=2)
-plot.show()
+
+print (softmax(scores))
+
+# Plot softmax curves
+#import matplotlib.pyplot as plt
+#x = np.arange(-2.0, 6.0, 0.1)
+#scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
+
+#plt.plot(x, softmax(scores).T, linewidth=2)
+#plot.show()
