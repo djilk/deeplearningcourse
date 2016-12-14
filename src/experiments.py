bad0f33e1787da7e3827440481904e275d84b5a9
Dave Jilk <dave@jilk.com>
2016-12-14 14:36:37 -0700

    Add experiments

---
+++ src/experiments.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+import tensorflow as tf
+
+print(tf.linspace(10.0, 12.0, 3))
+
+
