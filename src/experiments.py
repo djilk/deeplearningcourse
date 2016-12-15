eb3da02acfcdeb60082e7ea210ccbc131c1a8e1d
Dave Jilk <dave@jilk.com>
2016-12-15 11:18:13 -0700

    Create experiment_1, starting new experiment

--- src/experiments.py
+++ src/experiments.py
@@ -2,16 +2,23 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+# The goal of this is to create an "averaged histogram" function
+# but first do it for the Copper data
+
 import tensorflow as tf
 
 # Bins will be:  (-inf, 1), [1, 2), [2, 3), [3, 4), [4, inf)
 nbins = 5
 value_range = [0.0, 5.0]
-new_values = [-1.0, 0.0, 1.5, 2.0, 5.0, 15]
-#tf.initialize_all_variables().run()
-sess = tf.Session() 
+
+new_values = tf.constant([-1.0, 0.0, 1.5, 2.0, 5.0, 15], tf.float32)
+
+min = tf.reduce_min(new_values)
+max = tf.reduce_max(new_values)
+value_range = [max, min]
+
 hist = tf.histogram_fixed_width(new_values, value_range, nbins)
-print(sess.run(hist))
+print(tf.sess.run(hist))
   
   #=> [2, 1, 1, 0, 2]
 
