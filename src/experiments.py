e966e50dc62e2b2f57d2de29a3ababbab7db7e93
Dave Jilk <dave@jilk.com>
2016-12-15 10:42:48 -0700

    Create experiment_1, starting new experiment

--- src/experiments.py
+++ src/experiments.py
@@ -8,11 +8,10 @@ import tensorflow as tf
 nbins = 5
 value_range = [0.0, 5.0]
 new_values = [-1.0, 0.0, 1.5, 2.0, 5.0, 15]
-
-with tf.Session() as sess:
-  hist = tf.histogram_fixed_width(new_values, value_range, nbins)
-  variables.global_variables_initializer().run()
-  print(sess.run(hist))
+#tf.initialize_all_variables().run()
+sess = tf.Session() 
+hist = tf.histogram_fixed_width(new_values, value_range, nbins)
+print(sess.run(hist))
   
   #=> [2, 1, 1, 0, 2]
 
