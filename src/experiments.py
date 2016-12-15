ac4c4454c43a45bd4f8cc5b7a301b7ed59284e23
Dave Jilk <dave@jilk.com>
2016-12-15 11:21:30 -0700

    Create experiment_1, starting new experiment

--- src/experiments.py
+++ src/experiments.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 
 # Bins will be:  (-inf, 1), [1, 2), [2, 3), [3, 4), [4, inf)
 nbins = 5
-value_range = [0.0, 5.0]
+#value_range = [0.0, 5.0]
 
 new_values = tf.constant([-1.0, 0.0, 1.5, 2.0, 5.0, 15], tf.float32)
 
@@ -18,7 +18,8 @@ max = tf.reduce_max(new_values)
 value_range = [max, min]
 
 hist = tf.histogram_fixed_width(new_values, value_range, nbins)
-print(tf.Session.run(hist))
+sess = tf.Session()
+print(sess.run(hist))
   
   #=> [2, 1, 1, 0, 2]
 
